99da218cd830f2c3d65b38a626f90ff4b601fde8
Guilherme Hübner <guilhermehubner@gmail.com>
2022-04-30 10:15:01 +0200

    customizing tags

--- config.def.h
+++ config.def.h
@@ -19,15 +19,16 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5" };
+static const char *tags[] = { ">_ ", "www", "mus", "vid", "[?]" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	/* class            instance    title       tags mask          isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,       0,                 1,           -1 },
+	{ "Google-chrome",  NULL,       NULL,       1 << 1,            0,           -1 },
 };
 
 /* layout(s) */
