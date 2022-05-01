a17d0d75ff2892665d602909f8803f4741f15909
Guilherme Hübner <guilhermehubner@gmail.com>
2022-05-01 09:55:28 +0200

    improving border

--- config.def.h
+++ config.def.h
@@ -2,8 +2,8 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -14,7 +14,7 @@ static const char col_white[]       = "#f8f8f2";
 static const char col_purple[]      = "#bd93f9";
 static const char *colors[][3]      = {
 	/*               fg         bg          border   */
-	[SchemeNorm] = { col_white, col_gray1,  col_gray2 },
+	[SchemeNorm] = { col_white, col_gray1,  col_gray1 },
 	[SchemeSel]  = { col_white, col_purple, col_purple },
 };
 
